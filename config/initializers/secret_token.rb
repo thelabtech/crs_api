5af3ff3fe38aeddafb32d7b0751c5d3b41f34ce3
Josh Starcher <josh.starcher@gmail.com>
2012-04-13 09:08:02 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrsApi::Application.config.secret_token = '6b6025432699fbe69d48f311ba5e90623cdb2b8a645114d96490279198eca176e4f7b56cfccd5f13d4310a048e28aaaa29f8908b29e7175cb38bc058558dea4c'
